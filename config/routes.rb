c106a8463932c25e47838399e9add9f23d8f9173
brett <brettlorsch@gmail.com>
2018-03-19 12:01:29 -0600

    Added devise for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admins
   get 'pages/home'
   root to: 'pages#home'
 
@@ -11,4 +12,5 @@ Rails.application.routes.draw do
   resources :ingredients, only: [:new, :create, :edit, :update, :destroy]
   resources :sides, only: [:new, :create, :edit, :update, :destroy]
 
+  root to: "home#index"
 end
