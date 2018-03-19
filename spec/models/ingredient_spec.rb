2dacfd5ecdb51f4b4aeaa66884ea47c4fe27ac29
Parker <parkermatheson@gmail.com>
2018-03-19 13:31:30 -0600

    Adds first test to Ingredient model to verify that one ingredient is added to the database when one is created.

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Ingredient, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'has 1 ingredient after #create' do
+    Ingredient.create(:title => 'avocado', :price => '$1.00')
+    expect(Ingredient.count).to eq(1)
+  end
 end
