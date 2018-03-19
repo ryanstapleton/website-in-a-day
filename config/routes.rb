ed30841987f415b1bc26cdf0a84eeca92c58f66f
Ethan Streadbeck <estreadbeck@gmail.com>
2018-03-19 11:36:29 -0600

    Generated the Home, About us, and menu pages, with routes and a sandwich generated in seeds.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  root to: 'pages#home'
+
+  get 'about-us', to: 'pages#about'
+
+  get 'menu', to: 'pages#menu'
+
   resources :prices
   resources :sandwiches
   resources :ingredients
