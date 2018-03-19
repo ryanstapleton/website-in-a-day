1f5f899035c38078b5a304609063e82f64d5dbd3
Parker <parkermatheson@gmail.com>
2018-03-19 13:55:53 -0600

    Updates test for the default price of an ingredient

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -1,10 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Ingredient, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
 
-  it 'has 1 ingredient after #create' do
+  pending 'has 1 ingredient after #create' do
     Ingredient.create(:title => 'avocado', :price => '$1.00')
     expect(Ingredient.count).to eq(1)
   end
+
+  it 'has a default price of "" (an empty string)' do
+    Ingredient.create(:title => 'Tomatoes')
+    expect(Ingredient.last.price).to eq("")
+  end
+
+  pending 'has a price '
 end
