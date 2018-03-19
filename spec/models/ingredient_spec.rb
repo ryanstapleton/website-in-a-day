156ae41924ecda8139fab5d1a3643818b8b6ff14
Parker <parkermatheson@gmail.com>
2018-03-19 14:54:51 -0600

    Updates the default ingredient test to expect nil instead of an empty string

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -1,17 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Ingredient, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
 
-  pending 'has 1 ingredient after #create' do
+  xit 'has 1 ingredient after #create' do
     Ingredient.create(:title => 'avocado', :price => '$1.00')
     expect(Ingredient.count).to eq(1)
   end
 
-  it 'has a default price of "" (an empty string)' do
+  it 'has a default price of nil' do
     Ingredient.create(:title => 'Tomatoes')
-    expect(Ingredient.last.price).to eq("")
+    expect(Ingredient.last.price).to eq(nil)
   end
 
-  pending 'has a price '
+  it 'has a price '
 end
